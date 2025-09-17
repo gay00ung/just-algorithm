66eaa13298930457a2a3921810f77e43cce40eb8
gayoung <gayoung990911@gmail.com>
2025-09-17 12:56:08 +0900

    Initial Commit

--- baekjoon/build.gradle.kts
+++ baekjoon/build.gradle.kts
@@ -1,13 +1,20 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    kotlin("jvm") version "2.0.21"
+    application
 }
+
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
+
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
+}
+
+application {
+    // 실행할 메인 클래스 지정 (문제마다 바꿔주면 됨)
+    mainClass.set("")
 }
