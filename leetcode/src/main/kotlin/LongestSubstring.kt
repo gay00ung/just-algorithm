84cc9336c63101d1e4a4b165d4ed5b3a4064d81a
gayoung <gayoung990911@gmail.com>
2026-05-11 09:50:34 +0900

    03.LongestSubstring

---
+++ leetcode/src/main/kotlin/LongestSubstring.kt
@@ -0,0 +1,23 @@
+package org.example
+
+class Solution {
+    fun lengthOfLongestSubstring(s: String): Int {
+        var start = 0
+        var maxLength = 0
+        val seen = mutableMapOf<Char, Int>()
+
+        for (i in s.indices) {
+            val char = s[i]
+
+            if (seen.containsKey(char) && seen[char]!! >= start) {
+                start = seen[char]!! + 1
+            }
+
+            seen[char] = i
+
+            val currentLength = i - start + 1
+            maxLength = maxOf(maxLength, currentLength)
+        }
+        return maxLength
+    }
+}
