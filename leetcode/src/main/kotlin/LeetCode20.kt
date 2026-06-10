da68d49d1fabc06b3593234313806562c1143563
gayoung <gayoung990911@gmail.com>
2026-06-10 17:47:57 +0900

    20.valid-parentheses

--- leetcode/src/main/kotlin/LeetCode20.kt
+++ leetcode/src/main/kotlin/LeetCode20.kt
@@ -24,4 +24,31 @@ class LeetCode20 {
         }
         return stack.isEmpty()
     }
+
+    // 두번째 방법
+    fun isValid2(s: String): Boolean {
+        val stack = mutableListOf<Char>()
+        val map = mapOf(
+            ')' to '(',
+            ']' to '[',
+            '}' to '{'
+        )
+
+        for (ch in s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.add(ch)
+            } else {
+                if (stack.isEmpty()) {
+                    return false
+                }
+
+                val last = stack.removeAt(stack.lastIndex)
+
+                if (last != map[ch]) {
+                    return false
+                }
+            }
+        }
+        return stack.isEmpty()
+    }
 }
